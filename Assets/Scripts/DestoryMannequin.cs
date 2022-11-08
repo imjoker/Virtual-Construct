7580cf007b40ea5012d97f825c61f412d6f2187b
Yeshwanth Sai Kandula <yeshwanth251098@gmail.com>
2022-11-07 19:04:36 -0700

    Punch detected and other changes

--- Assets/Scripts/DestoryMannequin.cs
+++ Assets/Scripts/DestoryMannequin.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class DestoryMannequin : MonoBehaviour
 {
-    private GameObject mannequin;
+
     // Start is called before the first frame update
     void Start()
     {
-        mannequin = GetComponent<GameObject>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.CompareTag ("Player"))       
+            Destroy(this.gameObject);
     }
 
     // Update is called once per frame
