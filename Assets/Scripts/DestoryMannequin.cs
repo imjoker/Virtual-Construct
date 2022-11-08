cc607f98fbc0d669578fc5e6edc0946a22bb03b6
Yeshwanth Sai Kandula <yeshwanth251098@gmail.com>
2022-11-07 20:06:46 -0700

    WIP commit

--- Assets/Scripts/DestoryMannequin.cs
+++ Assets/Scripts/DestoryMannequin.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class DestoryMannequin : MonoBehaviour
 {
 
+    public GameObject block;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,13 +14,20 @@ public class DestoryMannequin : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag ("Player"))       
+        if (collision.gameObject.CompareTag("Player"))
+        {
+                float x;
+                float y;
+                float z;
+
+            x = this.gameObject.transform.position.x;
+            y = this.gameObject.transform.position.y;
+            z = this.gameObject.transform.position.z;
+
             Destroy(this.gameObject);
+
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
