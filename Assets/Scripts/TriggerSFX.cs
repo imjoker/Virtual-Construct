51ff4dc679e045d45adf8df138dd58dc480e7227
Yeshwanth Sai Kandula <yeshwanth251098@gmail.com>
2022-11-09 18:03:45 -0700

    Canvas with trashtalk images done

--- Assets/Scripts/TriggerSFX.cs
+++ Assets/Scripts/TriggerSFX.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerSFX : MonoBehaviour
 {
     public AudioSource trash_audio;
+    public Image       activate_img;
 
     private void OnTriggerEnter (Collider other)
     {
@@ -12,5 +14,8 @@ public class TriggerSFX : MonoBehaviour
 
         if (!trash_audio.isPlaying)
             trash_audio.Play();
+
+        if (!activate_img.IsActive ())
+            activate_img.gameObject.SetActive(true);
     }
 }
